df278e00c0814ab4356614b2fdd56d7942483153
Victor Olmedo <volmedo@es.com.py>
2023-09-26 17:46:25 -0400

    Create all side Menu links

--- asbap/billing/urls.py
+++ asbap/billing/urls.py
@@ -2,6 +2,32 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.BASE, name='BASE'),
+    path('', views.dashboard, name='dashboard'),
+    #Clients
+    path('addClient/', views.addClient, name='addClient'),
+    path('listClients/', views.listClients, name='listClients'),
+
+    #Products
+    path('addProduct/', views.addProduct, name='addProduct'),
+    path('listProducts/', views.listProducts, name='listProducts'),
+
+    # invoices
+    path('addInvoice/', views.addInvoice, name='addInvoice'),
+    path('listInvoices/', views.listInvoices, name='listInvoices'),
+
+    # Payments
+    path('addPayment/', views.addPayment, name='addPayment'),
+    path('listPayments/', views.listPayments, name='listPayments'),
+
+    # vendors
+    path('addVendor/', views.addVendor, name='addVendor'),
+    path('listVendors/', views.listVendors, name='listVendors'),
+
+    # Reports
+    path('reports/', views.reports, name='reports'),
+
+    # Settings
+    path('settings/', views.settings, name='settings'),
+
 ]
 
