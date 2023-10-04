f302433e61566bf05e42e9a8b9d6b23b0116c0d7
Victor Olmedo <volmedo@es.com.py>
2023-10-04 13:32:14 -0300

    RestFrameWork integration

--- asbap/asbap/settings.py
+++ asbap/asbap/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'asbap',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- asbap/billing/urls.py
+++ asbap/billing/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from rest_framework import routers
 from . import views
+
+
+router = routers.DefaultRouter()
+router.register(r'customers', views.CustomerViewSet)
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
